02b71b68867f37538d48d6e7faa3137e96ffdd82
WGtheLearner <2431073952@qq.com>
2022-07-14 23:52:03 +0800

    scene1-4

---
+++ project/GershgorinThm/WG/Proof2.py
@@ -0,0 +1,72 @@
+from manim import *
+
+
+class Proof2(Scene):
+    def construct(self):
+        #圈出第k行
+        Formula_3 = MathTex(r"\begin{bmatrix} a_{11} & ... & a_{1n}\\ ... &  & ... \\a_{k1} & ... & a_{kn} \\... &  & ...\\ a_{n1} & ... & a_{nn} \end{bmatrix} "
+                            r"\begin{bmatrix} x_1\\ ...\\ x_k \\ ... \\ x_n \end{bmatrix} = \lambda "
+                            r"\begin{bmatrix} x_1\\ ...\\ x_k \\ ... \\ x_n \end{bmatrix}").shift(UP * 2)
+        Formula_4 = MathTex(r"\lambda x_k", r"=", r"\sum_j", r"a_{kj}x_j")
+        Formula_5 = MathTex(r"\lambda x_k", r"=", r"\sum_{j\ne k}", r"a_{kj}x_j", r"+", r"a_{kk}x_k")
+        Formula_6 = MathTex(r"\lambda x_k", r" -a_{kk}x_k", r"=", r"\sum_{j"
+                                                                  r"x\ne k}", r"a_{kj}x_j")
+        Formula_7 = MathTex(r"(", r"\lambda", r"-a_{kk}", r")", r"x_k", r"=", r"\sum_{j\ne k}", r"a_{kj}x_j")
+        Formula_Hint_1 = MathTex(r"\triangle \ne: |a+b|\le|a|+|b|").next_to(Formula_7, DOWN)
+        Formula_8 = MathTex(r"|\lambda-a_{kk}||x_k|", r"=", r"|\sum_{j\ne k}", r"a_{kj}", r"x_j", r"|")
+        Formula_9 = MathTex(r"|\lambda-a_{kk}||x_k|", r"\le", r"\sum_{j\ne k}", r"|a_{kj}|", r"|", r"x_j", r"|")
+        Formula_10 = MathTex(r"|\lambda-a_{kk}||x_k|", r"\le", r"|a_{k1}|", r"|x_1|", r"+ ... +", r"|a_{k,k-1}|", r"|x_{k-1}|",
+                             r"+", r"|a_{k,k+1}|", r"|x_{k+1}|", r"+ ... +", r"|a_{k,n}|", r"|x_n| ").scale(0.8)
+        Formula_11 = MathTex(r"|\lambda-a_{kk}||x_k|", r"\le", r"|a_{k1}|", r"|x_k|", r"+ ... +", r"|a_{k,k-1}|",
+                             r"|x_k}|", r"+", r"|a_{k,k+1}|", r"|x_k|", r"+ ... +", r"|a_{k,n}|", r"|x_k| ").scale(0.8)
+        Formula_Hint_2 = MathTex(r"x \ is \ eigenvector, \ so \ |x_k|\ne 0").next_to(Formula_11, DOWN).scale(0.8)
+        Formula_12 = MathTex(r"|\lambda-a_{kk}||x_k|", r"\le", r"(\sum_{j\ne k} |a_{kj}|)", r"|x_k|")
+        Formula_13 = MathTex(r"|\lambda-a_{kk}|", r"\le", r"\sum_{j\ne k} |a_{kj}|")
+
+        self.play(Write(Formula_3))
+        self.wait()
+
+        self.play(Write(Formula_4.next_to(Formula_3, DOWN)))
+        self.wait()
+        self.play(Unwrite(Formula_3))
+        self.wait()
+        self.play(Formula_4.animate.shift(UP))
+        self.wait()
+
+        self.play(TransformMatchingTex(Formula_4, Formula_5))
+        self.wait()
+
+        self.play(TransformMatchingTex(Formula_5, Formula_6))
+        self.wait()
+
+        self.play(TransformMatchingTex(Formula_6, Formula_7))
+        self.wait()
+
+        self.play(TransformMatchingTex(Formula_7, Formula_8))
+        self.wait()
+
+        self.play(Write(Formula_Hint_1))
+        self.wait()
+        self.remove(Formula_Hint_1)
+        self.wait()
+
+        self.play(TransformMatchingTex(Formula_8, Formula_9))
+        self.wait()
+
+        self.play(TransformMatchingTex(Formula_9, Formula_10))
+        self.wait()
+
+        self.play(TransformMatchingTex(Formula_10, Formula_11))
+        self.wait()
+
+        self.play(TransformMatchingTex(Formula_11, Formula_12))
+        self.wait()
+
+        self.play(Write(Formula_Hint_2))
+        self.wait()
+        self.remove(Formula_Hint_2)
+        self.wait()
+
+        self.play(TransformMatchingTex(Formula_12, Formula_13))
+        self.wait()
+#manim -pql Proof2.py Proof2
--- project/GershgorinThm/WG/Test.py
+++ project/GershgorinThm/WG/Test.py
@@ -1,12 +1,49 @@
 from manim import *
-from numpy import linalg as LA
-import numpy as np
 
 
-class GDisk(Scene):
+class Proof1(Scene):
     def construct(self):
-        a = Tex("$x=y$")
-        self.play(Write(a))
+        title = Tex("Proof of Thm").shift(UP * 3.5)
+        Formula_1 = MathTex(r"Ax=\lambda x,\forall \lambda").shift(UP * 2.5)
+        Formula_2 = MathTex(r"Define \quad k=\arg\max_i |x_i|").next_to(Formula_1, DOWN)
+        #圈出最大行
+        Formula_2_1 = MathTex(r"\mbox{Example } x = \begin{bmatrix} 1\\ i \\ 2+i \end{bmatrix}, A").next_to(Formula_2, DOWN)
+        Formula_2_2 = MathTex(r"= \lambda \begin{bmatrix} 1\\ i \\ 2+i \end{bmatrix}")
+        matrix_X1 = Matrix([[1], ["i"], ["2+i"]], element_alignment_corner=DOWN)
+        matrix_X2 = Matrix([[1], ["i"], ["2+i"]], element_alignment_corner=DOWN)
+        MGroup = VGroup(Formula_2_1, matrix_X1, Formula_2_2).arrange_in_grid(cols=4)
 
+        rect = SurroundingRectangle(matrix_X1.get_rows()[2])
+        tex1 = MathTex(r"k = 3", color=YELLOW).scale(0.7).next_to(rect, DOWN)
+        self.play(Write(title))
 
-#manim -pql Test.py GDisk
+        self.play(Write(Formula_1))
+        self.wait()
+
+        self.play(Write(Formula_2))
+        self.wait()
+
+        self.play(Write(MGroup))
+        self.wait()
+        self.play(Create(rect))
+        self.wait()
+        self.play(Write(tex1))
+        self.wait()
+
+        self.remove(MGroup)
+
+        self.remove(Formula_1)
+        self.play(Formula_2.animate.next_to(title, DOWN))
+        self.play(Write(Formula_1.next_to(Formula_2, DOWN)))
+        self.wait()
+
+        Formula_3 = MathTex(r" = \lambda ")
+        matrix_A = Matrix([["a_{11}", "...", "a_{1n}"], ["...", "...", "..."], ["a_{k1}", "...", "a_{kn}"], ["...", "...", "..."], ["a_{n1}", "...", "a_{nn}"]], element_alignment_corner=DOWN)
+        matrix_X_1 = Matrix([["x_1"], ["..."], ["x_k"], ["..."], ["x_n"]], element_alignment_corner=DOWN)
+        matrix_X_2 = Matrix([["x_1"], ["..."], ["x_k"], ["..."], ["x_n"]], element_alignment_corner=DOWN)
+        MGroup2 = VGroup(matrix_A, matrix_X_1, Formula_3, matrix_X_2).arrange_in_grid(cols=4)
+        self.play(ReplacementTransform(Formula_1, MGroup2))
+        self.wait()
+        self.play(Create(SurroundingRectangle(matrix_A.get_rows()[2])))
+        self.play(Create(SurroundingRectangle(matrix_X_1.get_rows()[2])))
+#manim -pql Test.py Proof1
